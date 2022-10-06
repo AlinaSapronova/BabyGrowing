27bb97701645ff14e623c1316fd67a3badb939bc
alina <sapron4ik93@gmail.com>
2022-10-06 11:30:00 -0500

    Start

---
+++ baby-growing/src/App.js
@@ -0,0 +1,36 @@
+import React from "react"
+  
+
+import {
+
+  BrowserRouter as Router,
+
+  Routes,
+
+  Route,
+
+  Link
+
+} from "react-router-dom";
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+<nav>
+  <Link to=""></Link>
+  <Link to=""></Link>
+  <Link to=""></Link>
+  <Link to=""></Link>
+</nav>
+
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </Router>
+ 
+  );
+}
+
+export default App;
